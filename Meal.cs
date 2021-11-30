c37ec6a04af9a1582b913552e2dc00cd8dc64ab7
Piotr Stadnicki <88381909+sfeniks@users.noreply.github.com>
2021-11-30 18:13:53 +0100

    Add files via upload

---
+++ Meal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+     public class Meal
+    {
+        public Meal()
+        {
+            Random rand = new Random();
+            var x = rand.Next(1, 20);
+            var y = rand.Next(1, 20);
+            CurrentTarget = new Coordinate(x, y);
+            Draw();
+        }
+        public Coordinate CurrentTarget { get; set; }
+
+        void Draw()
+        {
+            //okodować aby nie mógł sie pojawić w węzu
+            Console.SetCursorPosition(CurrentTarget.X, CurrentTarget.Y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("$");
+        }
+        
+    }
+}
